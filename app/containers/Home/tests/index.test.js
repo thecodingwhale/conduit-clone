9ed0db2f9d97722c3ef9f33aeb6618b42baf21d1
Aldren Terante <aldrenterante@altus-dc.com>
2017-10-25 21:21:37 +0800

    create a <Home /> container and move <Articles /> container inside of it

--- app/containers/Home/tests/index.test.js
+++ app/containers/Home/tests/index.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Articles from 'containers/Articles';
+import PopularTags from 'containers/PopularTags';
 import { Home } from '../index';
 
 describe('<Home />', () => {
   it('should render <Articles /> container', () => {
     const component = shallow(<Home />);
-
     expect(component.find(Articles).length).toEqual(1);
+  });
+
+  it('should render <PopularTags /> container', () => {
+    const component = shallow(<Home />);
+    expect(component.find(PopularTags).length).toEqual(1);
   });
 });
