d765946dd66cbd14e9022abedba9eac32e399d2a
Aldren Terante <aldren.terante@gmail.com>
2017-11-19 21:21:53 +0800

    add redux-form

--- app/reducers.js
+++ app/reducers.js
@@ -5,6 +5,7 @@
 import { combineReducers } from 'redux-immutable';
 import { fromJS } from 'immutable';
 import { LOCATION_CHANGE } from 'react-router-redux';
+import { reducer as formReducer } from 'redux-form/immutable';
 
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
 
@@ -43,6 +44,7 @@ export default function createReducer(injectedReducers) {
   return combineReducers({
     route: routeReducer,
     language: languageProviderReducer,
+    form: formReducer,
     ...injectedReducers,
   });
 }
