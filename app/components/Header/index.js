023593bbd2ab893d89d6977d18390bd271f619a4
Aldren Terante <aldren.terante@gmail.com>
2017-11-28 18:05:56 +0800

    update <Header /> to handle authentication users

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -5,7 +5,9 @@
 */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { isEmpty } from 'lodash';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
@@ -21,6 +23,43 @@ class Header extends React.Component { // eslint-disable-line react/prefer-state
       isOpen: !this.state.isOpen,
     });
   }
+  renderLoggedOutView() {
+    return (
+      <Nav className="ml-auto" navbar>
+        <NavItem>
+          <NavLink href="/">Home</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/login">Login</NavLink>
+        </NavItem>
+      </Nav>
+    );
+  }
+  renderLoggedInView() {
+    return (
+      <Nav className="ml-auto" navbar>
+        <NavItem>
+          <NavLink href="/">Home</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/editor">
+            <i className="ion-compose"></i>&nbsp;New Post
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/settings">
+            <i className="ion-gear-a"></i>&nbsp;Settings
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href={`/author/@${this.props.currentUser.username}`}>
+            <img src={this.props.currentUser.image} className="user-pic" alt={this.props.currentUser.username} />
+            {this.props.currentUser.username}
+          </NavLink>
+        </NavItem>
+      </Nav>
+    );
+  }
   render() {
     return (
       <div>
@@ -28,14 +67,7 @@ class Header extends React.Component { // eslint-disable-line react/prefer-state
           <NavbarBrand href="/">Conduit</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/login">Login</NavLink>
-              </NavItem>
-            </Nav>
+            {isEmpty(this.props.currentUser) ? this.renderLoggedOutView() : this.renderLoggedInView()}
           </Collapse>
         </Navbar>
       </div>
@@ -44,7 +76,10 @@ class Header extends React.Component { // eslint-disable-line react/prefer-state
 }
 
 Header.propTypes = {
-
+  currentUser: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }),
 };
 
 export default Header;
