09dd2cfd2f578ade2409ab9e165e89da410edfbd
Aldren Terante <aldren.terante@gmail.com>
2017-12-22 14:20:38 +0800

    add react-storybook

---
+++ stories/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <button onClick={action('clicked')}>Hello Button</button>
+  ));
